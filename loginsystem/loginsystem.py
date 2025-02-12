b71b4bbc4ccecf0695b407886b1e72d081a98d66
jamilshehab <jamilshehab>
2025-02-12 21:08:18 +0200

    made the logic for username and password

--- loginsystem/loginsystem.py
+++ loginsystem/loginsystem.py
@@ -1,3 +1,7 @@
-
 username=input("enter your username : ")
 password=input("enter your password : ")
+
+if username == "admin" and password == "1234":
+   print("Access Granted")
+else:
+   print("Access Denied")
