ad7df1b3efebaeec941789993134d918580cd05d
jamilshehab <jamilshehab>
2025-02-12 20:37:42 +0200

    refactoring some code logic

--- movieticketcalculator/movieticketcalculator.py
+++ movieticketcalculator/movieticketcalculator.py
@@ -1,13 +1,13 @@
 age=int(input("enter your age : "))
 ticket=0
 
-if age >=12:
+if age<=14:
    ticket=12
    print("the price of the ticket is " , ticket , "$")
-elif age>=14:
+elif age <=18:
    ticket=14
    print("the ticket is " , ticket , "$")
-elif age >=18:
+elif age <=22:
    ticket=18
    print("the ticket is " , ticket , "$")
 else:
